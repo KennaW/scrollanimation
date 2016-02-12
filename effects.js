93796396e79cf8656a8bd25eae75b1d8b467d63c
KennaW <KennaWarsinske@gmail.com>
2016-02-12 00:22:41 -0800

    gh project pages test 1

---
+++ effects.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function() {
+
+  // $("#scroll").click(function() {
+  //     $('html, body').animate({
+  //         scrollTop: $("#endscroll").offset()
+  //     }, 2000);
+  // });
+
+
+  
+});
